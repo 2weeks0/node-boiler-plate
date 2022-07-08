f7657b70b1ad0ed70e70aabe3cde9cd3b595589c
2weeks0 <57306307+2weeks0@users.noreply.github.com>
2022-07-08 17:45:48 +0900

    refactor(client): 회원가입 리팩토링
    나누어져있던 state 문맥에 맞게 합침

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -9,13 +9,16 @@ import {
   Grid,
   Button,
   Link,
+  FormHelperText,
+  FormControl,
 } from "@mui/material";
-import React, { FC, useState } from "react";
+import React, { FC, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { registerThunk } from "../store/slices/userSlice";
 
 interface RegisterForm {
+  [index: string]: string | boolean;
   email: string;
   password: string;
   name: string;
@@ -23,11 +26,18 @@ interface RegisterForm {
   agree: boolean;
 }
 
+interface RegisterError {
+  [index: string]: Error;
+  email: Error;
+  password: Error;
+  name: Error;
+  confirmPassword: Error;
+  agree: Error;
+}
+
 interface Error {
-  email: boolean;
-  password: boolean;
-  name: boolean;
-  confirmPassword: boolean;
+  value: boolean;
+  msg: string;
 }
 
 interface IProps {}
@@ -40,28 +50,80 @@ export const RegisterPage: FC<IProps> = (props) => {
     confirmPassword: "",
     agree: false,
   });
-  const [error, setError] = useState<Error>({
-    email: false,
-    password: false,
-    name: false,
-    confirmPassword: false,
+  const [error, setError] = useState<RegisterError>({
+    email: {
+      value: false,
+      msg: "",
+    },
+    password: {
+      value: false,
+      msg: "",
+    },
+    name: {
+      value: false,
+      msg: "",
+    },
+    confirmPassword: {
+      value: false,
+      msg: "",
+    },
+    agree: {
+      value: false,
+      msg: "",
+    },
   });
+  const inputRefs = useRef<any>([]);
 
   const dispatch = useDispatch<any>();
   const navigate = useNavigate();
 
+  const onChangeValue = (key: string, v: string | boolean) => {
+    setForm((prev) => ({
+      ...prev,
+      [key]: v,
+    }));
+    setError((prev) => ({
+      ...prev,
+      [key]: {
+        value: !v,
+        msg: !v ? `${key}을(를) 입력하세요.` : "",
+      },
+    }));
+  };
+
+  const onKeyPress = (event: React.KeyboardEvent, index: number) => {
+    if (index === Object.keys(form).length - 1) {
+      inputRefs.current[index].blur();
+      return onSubmit();
+    }
+
+    if (event.key === "Enter") {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
   const onSubmit = async () => {
-    if (!form.email || !form.password || !form.name || !form.confirmPassword) {
+    if (!form.email || !form.password || !form.name || !form.confirmPassword || !form.agree) {
+      Object.keys(form).forEach((key) =>
+        setError((prev) => ({
+          ...prev,
+          [key]: {
+            value: !form[key],
+            msg: !form[key] ? `${key}을(를) 입력하세요.` : "",
+          },
+        }))
+      );
       return;
     }
 
-    if (!form.agree) {
-      return alert("이용 약관에 동의해주세요.");
-    }
-
     if (form.password !== form.confirmPassword) {
-      setError({ ...error, password: true, confirmPassword: true });
-      return alert("비밀번호와 비밀번호 확인이 다릅니다.");
+      const errorMsg = "비밀번호와 비밀번호 확인이 다릅니다.";
+      setError((prev) => ({
+        ...prev,
+        password: { value: true, msg: errorMsg },
+        confirmPassword: { value: true, msg: errorMsg },
+      }));
+      return;
     }
 
     const result = await dispatch(
@@ -79,54 +141,37 @@ export const RegisterPage: FC<IProps> = (props) => {
     <RegisterPagePresenter
       form={form}
       error={error}
-      onChangeEmail={(v) => setForm({ ...form, email: v })}
-      onChangePassword={(v) => setForm({ ...form, password: v })}
-      onChangeName={(v) => setForm({ ...form, name: v })}
-      onChangeConfirmPassword={(v) => setForm({ ...form, confirmPassword: v })}
-      onChangeAgree={(v) => setForm({ ...form, agree: v })}
+      onChangeValue={onChangeValue}
       onSubmit={onSubmit}
-      setErrorEmail={(v) => setError({ ...error, email: v })}
-      setErrorPassword={(v) => setError({ ...error, password: v })}
-      setErrorConfirmPassword={(v) => setError({ ...error, confirmPassword: v })}
-      setErrorName={(v) => setError({ ...error, name: v })}
+      inputRefs={inputRefs}
+      onKeyPress={onKeyPress}
     />
   );
 };
 
 interface IPresenterProps {
   form: RegisterForm;
-  error: Error;
-  onChangeEmail: (v: string) => void;
-  onChangePassword: (v: string) => void;
-  onChangeName: (v: string) => void;
-  onChangeConfirmPassword: (v: string) => void;
-  onChangeAgree: (v: boolean) => void;
+  error: RegisterError;
+  onChangeValue: (key: string, v: string | boolean) => void;
   onSubmit: () => Promise<void>;
-  setErrorEmail: (v: boolean) => void;
-  setErrorPassword: (v: boolean) => void;
-  setErrorConfirmPassword: (v: boolean) => void;
-  setErrorName: (v: boolean) => void;
+  inputRefs: React.MutableRefObject<any>;
+  onKeyPress: (event: React.KeyboardEvent, index: number) => void;
 }
 
 export const RegisterPagePresenter: FC<IPresenterProps> = ({
   form,
   error,
-  onChangeEmail,
-  onChangePassword,
-  onChangeName,
-  onChangeConfirmPassword,
-  onChangeAgree,
+  onChangeValue,
   onSubmit,
-  setErrorEmail,
-  setErrorPassword,
-  setErrorConfirmPassword,
-  setErrorName,
+  inputRefs,
+  onKeyPress,
 }) => {
   return (
     <Container
       maxWidth="xs"
       sx={{
-        height: "100vh",
+        marginTop: 10,
+        marginBottom: 10,
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
@@ -149,10 +194,11 @@ export const RegisterPagePresenter: FC<IPresenterProps> = ({
         fullWidth
         autoFocus
         value={form.email}
-        onChange={(e) => onChangeEmail(e.target.value)}
-        error={error.email}
-        helperText={error.email ? "이메일을 입력하세요." : ""}
-        onBlur={() => setErrorEmail(!form.email)}
+        onChange={(e) => onChangeValue("email", e.target.value)}
+        error={error.email.value}
+        helperText={error.email.msg}
+        onKeyUp={(e) => onKeyPress(e, 0)}
+        inputRef={(ref) => (inputRefs.current[0] = ref)}
       />
 
       <TextField
@@ -163,10 +209,11 @@ export const RegisterPagePresenter: FC<IPresenterProps> = ({
         required
         fullWidth
         value={form.password}
-        onChange={(e) => onChangePassword(e.target.value)}
-        error={error.password}
-        helperText={error.password ? "비밀번호를 입력하세요." : ""}
-        onBlur={() => setErrorPassword(!form.password)}
+        onChange={(e) => onChangeValue("password", e.target.value)}
+        error={error.password.value}
+        helperText={error.password.msg}
+        onKeyUp={(e) => onKeyPress(e, 1)}
+        inputRef={(ref) => (inputRefs.current[1] = ref)}
       />
 
       <TextField
@@ -177,10 +224,11 @@ export const RegisterPagePresenter: FC<IPresenterProps> = ({
         required
         fullWidth
         value={form.confirmPassword}
-        onChange={(e) => onChangeConfirmPassword(e.target.value)}
-        error={error.confirmPassword}
-        helperText={error.confirmPassword ? "비밀번호 확인을 입력하세요." : ""}
-        onBlur={() => setErrorConfirmPassword(!form.confirmPassword)}
+        onChange={(e) => onChangeValue("confirmPassword", e.target.value)}
+        error={error.confirmPassword.value}
+        helperText={error.confirmPassword.msg}
+        onKeyUp={(e) => onKeyPress(e, 2)}
+        inputRef={(ref) => (inputRefs.current[2] = ref)}
       />
 
       <TextField
@@ -191,19 +239,28 @@ export const RegisterPagePresenter: FC<IPresenterProps> = ({
         required
         fullWidth
         value={form.name}
-        onChange={(e) => onChangeName(e.target.value)}
-        error={error.name}
-        helperText={error.name ? "이름을 입력하세요." : ""}
-        onBlur={() => setErrorName(!form.name)}
+        onChange={(e) => onChangeValue("name", e.target.value)}
+        error={error.name.value}
+        helperText={error.name.msg}
+        onKeyUp={(e) => onKeyPress(e, 3)}
+        inputRef={(ref) => (inputRefs.current[3] = ref)}
       />
 
       <Grid container>
-        <FormControlLabel
-          control={
-            <Checkbox checked={form.agree} onChange={(e) => onChangeAgree(e.target.checked)} />
-          }
-          label="이용 약관에 동의합니다."
-        />
+        <FormControl error={error.agree.value}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={form.agree}
+                onChange={(e) => onChangeValue("agree", e.target.checked)}
+              />
+            }
+            label="이용 약관에 동의합니다."
+            onKeyUp={(e) => onKeyPress(e, 4)}
+            inputRef={(ref) => (inputRefs.current[4] = ref)}
+          />
+          <FormHelperText>{error.agree.msg}</FormHelperText>
+        </FormControl>
       </Grid>
 
       <Button variant="contained" fullWidth onClick={onSubmit} sx={{ m: 2 }}>
@@ -211,7 +268,7 @@ export const RegisterPagePresenter: FC<IPresenterProps> = ({
       </Button>
 
       <Grid container>
-        <Grid item xs/>
+        <Grid item xs />
         <Link href="/login">로그인</Link>
       </Grid>
     </Container>
